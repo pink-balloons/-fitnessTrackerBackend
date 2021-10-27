d24a08619072a212bc634af1f20f8cbf342a07a2
Robert <robwoolbright@gmail.com>
2021-10-27 19:27:47 -0400

    created drop and create tables, but not working

--- db/client.js
+++ db/client.js
@@ -1,3 +1,18 @@
 // build and export your unconnected client here
 
 // some cool coding stuff
+
+const { Client } = require("pg");
+
+const CONNECTION_STRING =
+  process.env.DATABASE_URL || "postgres://localhost:3000/fitness-dev";
+
+const client = new Client({
+  connectionString: CONNECTION_STRING,
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : undefined,
+});
+
+module.exports = client;
--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // create the express server here
+
+require("dotenv").config();
+
+const { PORT = 3000 } = process.env;
+
+const express = require("express");
+const server = express();
+
+const morgan = require("morgan");
+server.use(morgan("dev"));
+
+server.use(express.json());
+
+server.listen(PORT, () => {
+  console.log("The server is up on port", PORT);
+});
