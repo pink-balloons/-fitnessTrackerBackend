50045c3c6328413b4fa44db308d63509a09cb287
Robert <robwoolbright@gmail.com>
2021-10-27 21:03:53 -0400

    template for routines.js

--- db/seedData.js
+++ db/seedData.js
@@ -1,5 +1,5 @@
 // require in the database adapter functions as you write them (createUser, createActivity...)
-// const { } = require('./');
+const { createUser } = require("./");
 const client = require("./client");
 
 async function dropTables() {
@@ -7,6 +7,7 @@ async function dropTables() {
   // drop all tables, in the correct order
   try {
     await client.query(`
+      DROP TABLE IF EXISTS routine_activities;
       DROP TABLE IF EXISTS routines;
       DROP TABLE IF EXISTS activities;
       DROP TABLE IF EXISTS users;
@@ -27,7 +28,7 @@ async function createTables() {
       CREATE TABLE users (
         id SERIAL PRIMARY KEY,
         username varchar(255) UNIQUE NOT NULL,
-        password varchar(255) NOT NULL,
+        password varchar(255) NOT NULL
       );
     `);
 
@@ -35,17 +36,26 @@ async function createTables() {
       CREATE TABLE activities (
         id SERIAL PRIMARY KEY,
         name varchar(255) UNIQUE NOT NULL,
-        description TEXT NOT NULL,
+        description TEXT NOT NULL
+      );
+    `);
+    await client.query(`
+      CREATE TABLE routines (
+        id SERIAL PRIMARY KEY,
+        "creatorId" INTEGER REFERENCES users(id),
+        "isPublic" Boolean DEFAULT false,
+        name varchar(255) UNIQUE NOT NULL,
+        goal TEXT NOT NULL
       );
     `);
 
     await client.query(`
       CREATE TABLE routines (
-        id SERIAL PRIMARY KEY,
-        "creatorId" INTEGER FOREIGN KEY,
-        "isPublic" Boolean DEFAULT false,
-        name varchar(255) UNIQUE NOT NULL,
-        goal TEXT NOT NULL,
+      id SERIAL PRIMARY KEY,
+      "routineId" INTEGER REFERENCES routines(id),
+      "activityId" INTEGER REFERENCES activities(id), 
+      duration INTEGER, 
+      count INTEGER
       );
     `);
 
@@ -236,9 +246,9 @@ async function rebuildDB() {
     await dropTables();
     await createTables();
     await createInitialUsers();
-    await createInitialActivities();
-    await createInitialRoutines();
-    await createInitialRoutineActivities();
+    // await createInitialActivities();
+    // await createInitialRoutines();
+    // await createInitialRoutineActivities();
   } catch (error) {
     console.log("Error during rebuildDB");
     throw error;
